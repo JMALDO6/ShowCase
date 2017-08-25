3ba54d0bf0b20a32ad343c8799d049643100b029
jmaldonadoa <jmaldonadoa@intergrupo.com>
2017-08-25 14:07:14 -0500

    Se agrega ViewModel

---
+++ ShowCase/ShowCase/View/InicioPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using ShowCase.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ShowCase.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class InicioPage : ContentPage
+    {
+        InicioViewModel Contexto = new InicioViewModel();
+
+        public InicioPage()
+        {
+            InitializeComponent();
+            BindingContext = Contexto;
+        }
+    }
+}
---
+++ ShowCase/ShowCase/ViewModel/InicioViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShowCase.ViewModel
+{
+    public class InicioViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string nombre = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+
+        public Command CamaraCommand { get; set; }
+        public Command GpsCommand { get; set; }
+
+        private bool isBusy = false;
+        public bool IsBusy
+        {
+            get { return isBusy = false; }
+            set
+            {
+                isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public InicioViewModel()
+        {
+            //CamaraCommand = new Command(async () => await Guardar(), () => !IsBusy);
+            //GpsCommand = new Command(async () => await Modificar(), () => !IsBusy);
+        }
+    }
+}
